6ff2b6d764a39ce111cd55516242396fbcc32fa2
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-26 23:48:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase==phrase.upcase
-    return "NO, NOT SINCE 1938!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
-  end
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase=="NO, not since 1938!"
+  else "HUH?! SPEAK UP, SONNY!"
+  end 
 end
